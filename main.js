9f6665bded78f56f2c1024aa12aadbd1ba964117
BryanNtust <151358793+BryanNtust@users.noreply.github.com>
2023-12-03 21:10:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    var element = document.getElementById("sec-1373");
+    element.classList.toggle("dark-mode");
+    var element = document.getElementById("sec-8810");
+    element.classList.toggle("dark-mode");
+}
